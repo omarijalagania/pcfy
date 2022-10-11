26b933cea85483c074166689ac49c3adc2eed1e2
omari <omari.jalagania@gmail.com>
2022-10-11 13:39:46 +0400

    feat: add main page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,21 @@
+<script>
+import TheButton from "./shared/TheButton.vue"
+export default {
+  components: {
+    TheButton,
+  },
+}
+</script>
+
+<template>
+  <div class="h-screen">
+    <div class="flex pt-20 pb-24 flex-col justify-center items-center">
+      <img alt="Vue logo" src="../assets/images/logo.png" />
+      <img class="mt-44" src="../assets/images/main.png" alt="main image" />
+      <div class="mt-28 flex flex-col space-y-6">
+        <TheButton name="ᲩᲐᲜᲐᲬᲔᲠᲘᲡ ᲓᲐᲛᲐᲢᲔᲑᲐ" />
+        <TheButton name="ᲩᲐᲜᲐᲬᲔᲠᲘᲡ ᲓᲐᲛᲐᲢᲔᲑᲐ" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/shared/TheButton.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  props: ["name"],
+}
+</script>
+
+<template>
+  <button class="w-96 h-14 bg-blue-400 rounded-md text-white">
+    {{ name }}
+  </button>
+</template>
