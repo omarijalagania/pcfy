1deab0119feb2d7e995f4e74c0dd435477dc44c0
omari <omari.jalagania@gmail.com>
2022-10-18 17:23:04 +0400

    feat: add success page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,11 @@
 import HomePage from "../views/HomePage"
 import ClientInfo from "../views/ClientInfo"
 import PcInfo from "../views/PcInfo.vue"
+import AddSuccess from "../views/AddSuccess.vue"
 
 export const routes = [
   { path: "/", component: HomePage },
   { path: "/client-info", component: ClientInfo },
   { path: "/pc-info", component: PcInfo },
+  { path: "/success", component: AddSuccess },
 ]
---
+++ src/views/AddSuccess.vue
@@ -0,0 +1,19 @@
+<script>
+import TheButton from "../components/shared/TheButton.vue"
+
+export default {
+  components: { TheButton },
+}
+</script>
+
+<template>
+  <div class="h-screen flex flex-col items-center">
+    <img class="object-cover" src="../assets/images/frame.png" alt="frame" />
+    <h2 class="font-bold text-2xl mb-16">ჩანაწერი დამატებულია!</h2>
+    <TheButton width="w-[297px]" name="სიაში გადაყვანა" />
+
+    <router-link class="mt-7 font-medium text-xl text-[#62A1EB]" to="/"
+      >მთავარი</router-link
+    >
+  </div>
+</template>
